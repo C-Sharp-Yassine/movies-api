c9cbab860271de2e439850c59a0065994865c443
C-Sharp-Yassine <153088430+C-Sharp-Yassine@users.noreply.github.com>
2026-09-14 12:04:22 +0200

    chore: set up app and database

---
+++ src/db/database.js
@@ -0,0 +1,16 @@
+import Database from "better-sqlite3";
+
+const db = new Database("movies.db");
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  genre TEXT NOT NULL,
+  director TEXT NOT NULL,
+  year INTEGER NOT NULL
+
+  );
+`);
+
+export default db;
