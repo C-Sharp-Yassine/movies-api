662395a61351986077c0854a142e3881ac91f11e
C-Sharp-Yassine <153088430+C-Sharp-Yassine@users.noreply.github.com>
2026-09-14 20:09:42 +0200

    feat: add CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import moviesRouter from "./routes/movies.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/movies", moviesRouter);
